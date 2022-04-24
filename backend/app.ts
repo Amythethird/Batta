680e7c353cbad9b0d28097509d17c7c551cfc983
Denise Büning <denise24111993@gmail.com>
2022-04-24 19:10:07 +0200

    Update app.ts

--- backend/app.ts
+++ backend/app.ts
@@ -4,3 +4,4 @@ const port=5000
 const url=`http://localhost:${port}`
 console.log(url);
 await serve((_req) => new Response("Hello WTW Team.  This is the DENO backend\n"), {port});
+console.log("Hello")
