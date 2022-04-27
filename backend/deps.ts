4d0ea58b3213fff7e163f33dba690458bd380e7c
Aljosha Merz <acmerz@posteo.de>
2022-04-27 16:08:02 +0200

    rm arangodb

--- backend/deps.ts
+++ backend/deps.ts
@@ -1,6 +1,4 @@
 import { Application,Router as R} from "https://deno.land/x/oak@v10.5.1/mod.ts";
-import { Arango as ArangoDb} from "https://deno.land/x/darango@0.1.6/mod.ts";
 
 export const Server=new Application()
 export const Router=R
-export const Arango=ArangoDb
--- backend/src/routes/shop.ts
+++ backend/src/routes/shop.ts
@@ -1,13 +1,5 @@
-import {getDocument} from "../database.ts";
-const prefix='shop'
-const collection='shops'
-
 export default function register(router:any){
-    router.get(`/${prefix}`,async (ctx:any)=>{
-        ctx.response.body=await getDocument(collection)
-    })
-
-    router.get(`/${prefix}/:key`,async (ctx:any)=>{
-        ctx.response.body=await getDocument(collection, ctx.params.key)
+    router.get(`/`,async (ctx:any)=>{
+        ctx.response.body='BLAH'
     })
 }
