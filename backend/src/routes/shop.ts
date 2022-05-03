7611bf2ce8be910b0d5d658bc3c7a18c541c88eb
Aljosha Merz <acmerz@posteo.de>
2022-05-03 22:05:05 +0200

    fix:undefined url in Deno

--- backend/src/routes/shop.ts
+++ backend/src/routes/shop.ts
@@ -1,7 +1,11 @@
 import {fetchData} from "../commonFunctions.ts";
+const prefix='shop'
 
 export default function register(router:any){
-    router.get(`/`,async (ctx:any)=>{
+    router.get(`/${prefix}s`,async (ctx:any)=>{
         ctx.response.body=await fetchData('get','shops')
+    })
+    router.get(`/${prefix}/:id`,async (ctx:any)=>{
+        ctx.response.body=await fetchData('get',`shops/${ctx.params.id}`)
     })
 }
