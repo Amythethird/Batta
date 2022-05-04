b1aa10c1569bde9b13521ae7b99b29f15c11ab8c
Melvin Weiershäuser <51531071+mweiershaeuser@users.noreply.github.com>
2022-05-04 11:50:09 +0200

    Revert "formatted project files with prettier"
    This reverts commit d80f694a9dbe58b2e60560958595d939d78b875a.

--- docker/strapi/veedelsretter/src/api/shop/services/shop.js
+++ docker/strapi/veedelsretter/src/api/shop/services/shop.js
@@ -1,9 +1,9 @@
-"use strict";
+'use strict';
 
 /**
  * shop service.
  */
 
-const { createCoreService } = require("@strapi/strapi").factories;
+const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService("api::shop.shop");
+module.exports = createCoreService('api::shop.shop');
