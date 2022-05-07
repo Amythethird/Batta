46cec7e2c4a0f2cdce5cffca0d093fa40f388e36
Denise Büning <denise24111993@gmail.com>
2022-05-07 20:18:47 +0200

    chore: implement bulma and uninstall tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,39 @@
-//import React from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import "./styles/style.css"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Dies ist ein  normaler p Tag
-        </p>
-        <li>Die ist ein listen element</li>
-        <a
-          className="App-link"
-          href="https://ads-medienmanufaktur.de"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Ich bin ein a Tag
-        </a>
-      </header>
-    </div>
+    <header className="AppHeader">
+    <nav className="navbar is-fixed-top has-shadow" aria-label="Main navigation">
+      <div className="container">
+        <div className="navbar-brand">
+          <a className="navbar-item" href="/">
+            <img src="/assets/logo.svg" alt="Back home" height={ 28 }/>
+          </a>
+
+          <button
+              role="button"
+              className="navbar-burger"
+              aria-label="Main menu"
+              aria-expanded="false"
+          >
+            <span aria-hidden="true"/>
+            <span aria-hidden="true"/>
+            <span aria-hidden="true"/>
+          </button>
+        </div>
+
+        <div className="navbar-menu">
+          <div className="navbar-start">
+            <a className="navbar-item" href="#about">About</a>
+            <a className="navbar-item" href="#foo">Foo</a>
+            <a className="navbar-item" href="#bar">Bar</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </header>
   );
 }
 
