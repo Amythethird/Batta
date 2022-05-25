29179139ed8c1ea20d9e5bdd799ebf5c9479b1a4
Amythethird <38221157+Amythethird@users.noreply.github.com>
2022-05-25 15:45:32 +0200

    Create Login and Shops  (#90)
    * Create Login and Shops 
    
    Basic Data Interface for Shops integrated
    
    * Update: Responsive Group
    
    * Update: Components
    
    Sections and Container updated

--- frontend/src/components/layout/Main.tsx
+++ frontend/src/components/layout/Main.tsx
@@ -11,7 +11,21 @@ return (
     <Routes>
       <Route path='/' element={<Home/>} />
       <Route path='/login' element={<Login/>}></Route>
-      <Route path='/shops' element={<Shop name='Test'/>}></Route>
+      <Route path='/shops' element={<Shop 
+      name='Test' 
+      text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis inventore consequuntur eos, 
+      quaerat est qui saepe ducimus pariatur alias, ipsum ea impedit. 
+      Et iste veniam debitis porro alias nobis officiis.'
+      oeffnungszeiten='Mo-Fr: 09:00 Uhr - 18:00 Uhr -- Sa 10:00 Uhr - 16:00 Uhr'
+      adresse='Musterstr. 1 -- 
+      12345 Musterstadt'
+      tag={['Unverpackt', 'Vegan', 'Nachhaltig']}
+      mail= 'info@gibtesnicht.de'
+      tel='015155255663'
+
+      
+      />}></Route>
+      
     </Routes>
 );
 }
