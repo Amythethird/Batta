85c56b53ef5da66e7eb5d9bc22016cde5b3ead59
Aljosha Merz <acmerz@posteo.de>
2022-06-05 15:45:03 +0200

    Add simulation for request getting and posting arbitrary data from react
    Remove unneeded files interfaces and shop

--- backend/deps.ts
+++ backend/deps.ts
@@ -2,6 +2,10 @@ import {
   Application,
   Router as R,
 } from "https://deno.land/x/oak@v10.5.1/mod.ts";
+import * as fileSystem from "https://deno.land/std@0.142.0/fs/mod.ts";
+import * as path from "https://deno.land/std@0.141.0/path/mod.ts";
 
 export const Server = new Application();
 export const Router = R;
+export const PATH = path;
+export const fs = fileSystem;
